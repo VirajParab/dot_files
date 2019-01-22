fb6d3426c5c656405216cdedbb3faf59b59d24fd
Viraj Parab <virajpa@thoughtworks.com>
2019-01-22 10:03:39 +0530

    Viraj - Update emacs script to support go lang

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,7 +21,7 @@
      ("gnu" . "https://elpa.gnu.org/packages/"))))
  '(package-selected-packages
    (quote
-    (find-file-in-project neotree go-mode linum-relative indent-tools airline-themes dracula-theme protobuf-mode))))
+    (fzf hydra ivy popup powerline s yafolding auto-complete exec-path-from-shell go-autocomplete find-file-in-project neotree go-mode linum-relative indent-tools airline-themes dracula-theme protobuf-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -35,6 +35,25 @@
 
 (require 'linum-relative)
 (linum-relative-global-mode)
+(setq linum-relative-current-symbol "")
 
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
+
+(add-to-list 'exec-path "/Users/Viraj_Kumar/work/bin")
+(add-hook 'before-save-hook 'gofmt-before-save)
+(defun my-go-mode-hook ()
+  ; Call Gofmt before saving                                                    
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Godef jump key binding                                                      
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "M-*") 'pop-tag-mark)
+  )
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+(defun auto-complete-for-go ()
+  (auto-complete-mode 1))
+(add-hook 'go-mode-hook 'auto-complete-for-go)
+(with-eval-after-load 'go-mode
+  (require 'go-autocomplete))
+
